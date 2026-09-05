612d0796b1a55646634d1fe6dc8a7d7f29e18b24
LuHugo <lyyonlive@gmail.com>
2026-09-05 21:59:34 +0800

    fix: use loader instead of beforeLoad for root redirect to prevent hover refresh

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
-  beforeLoad: () => {
+  loader: () => {
     throw redirect({
       to: '/$lang',
       params: {
