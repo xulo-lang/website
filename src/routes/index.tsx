01e6e472b800dfa5989bbe18cc686f77b064b06e
LuHugo <lyyonlive@gmail.com>
2026-09-05 22:05:28 +0800

    Revert "fix: remove root route to prevent infinite refresh"
    This reverts commit 53576cc4d8f15dc3bb7757590a136cdad900e2fe.

---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { createFileRoute, redirect } from '@tanstack/react-router';
+
+export const Route = createFileRoute('/')({
+  beforeLoad: () => {
+    throw redirect({
+      to: '/$lang',
+      params: {
+        lang: 'en',
+      },
+    });
+  },
+});
