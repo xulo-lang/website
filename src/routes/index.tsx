4681448cc3ce91c69532e315a93a29dd42691468
LuHugo <lyyonlive@gmail.com>
2026-09-05 22:01:27 +0800

    fix: remove redirect from docs loader to prevent infinite refresh

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
-  loader: () => {
+  beforeLoad: () => {
     throw redirect({
       to: '/$lang',
       params: {
